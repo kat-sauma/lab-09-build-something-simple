8027a829ee96f745fa3b1cf0d97741d7ff8dc6ef
Kat Sauma <katsauma@gmail.com>
2021-04-07 10:11:57 -0700

    POST route written with controller and model and test goes green

--- __tests__/app.test.js
+++ __tests__/app.test.js
@@ -2,9 +2,31 @@ const pool = require('../lib/utils/pool');
 const setup = require('../data/setup');
 const request = require('supertest');
 const app = require('../lib/app');
+const Dancer = require('../lib/models/Dancer');
 
 describe('build-something-simple routes', () => {
   beforeEach(() => {
     return setup(pool);
   });
+
+  let dancer;
+  beforeEach(async () => {
+    dancer = await Dancer.insert({  name: 'Bebe Miller', genre: 'Post-Modern', method: 'True', country: 'USA' });
+
+  });
+
+  it ('adds a choreographer to the database', async () => {
+    const res = await request(app)
+      .post('/api/v1/dancers')
+      .send({ name: 'Bebe Miller', genre: 'Post-Modern', method: 'True', country: 'USA' });
+
+    expect(res.body).toEqual({ 
+      id: expect.any(String),
+      name: 'Bebe Miller', 
+      genre: 'Post-Modern', 
+      method: true, 
+      country: 'USA'
+    });
+  });
+
 });
